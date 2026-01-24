b4ae379cde6b8a8deb1883b4bd3062a2224bfb5b
茶狐 <fox_tea@outlook.com>
2026-01-24 21:20:37 +0800

    加入了测试视频，将开启视频测试

---
+++ script/aruco_2.py
@@ -0,0 +1,20 @@
+import numpy as np
+from .base_overlay import load_media, draw_plane
+
+video_caps = {}
+
+MARKER_ID = 2
+MEDIA_PATH = "script/image/2.mp4"
+
+def render(frame, rvec, tvec, K, dist):
+    media_frame, alpha = load_media(MEDIA_PATH, video_caps, MARKER_ID)
+
+    # 悬浮在上方 + 倾斜
+    obj_pts = np.float32([
+        [-0.05, 0.10, 0.02],
+        [ 0.05, 0.10, 0.02],
+        [ 0.05, 0.02, 0.02],
+        [-0.05, 0.02, 0.02],
+    ])
+
+    return draw_plane(frame, media_frame, alpha, obj_pts, rvec, tvec, K, dist)
