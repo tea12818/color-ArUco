7c67c0390fa6efa6c2ee2eff5074dd7eae780ad2
茶狐 <fox_tea@outlook.com>
2026-01-24 21:25:05 +0800

    解决了bug

--- script/aruco_2.py
+++ script/aruco_2.py
@@ -1,14 +1,12 @@
 import numpy as np
 from .base_overlay import load_media, draw_plane
 
-
 MARKER_ID = 2
 MEDIA_PATH = "script/image/2.mp4"
 
 def render(frame, rvec, tvec, K, dist):
-    media_frame, alpha = load_media(MEDIA_PATH, video_caps, MARKER_ID)
+    media_frame, alpha = load_media(MEDIA_PATH, MARKER_ID)
 
-    # 悬浮在上方 + 倾斜
     obj_pts = np.float32([
         [-0.05, 0.10, 0.02],
         [ 0.05, 0.10, 0.02],
